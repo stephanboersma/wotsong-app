fba9a7b12295c226a0dba555126780818c43e5b7
Stephan Boersma <boersma@live.dk>
2022-04-11 14:38:37 +0200

    Add colour and font in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        champagne: '#F8E9A1',
+        coral: '#F76C6C',
+        uranus: '#A8D0E6',
+        cornflower: '#374786',
+        nightsky: '#24306E',
+      },
+      fontFamily: {
+        Baloo: ['Baloo 2', 'cursive'],
+      },
+    },
   },
   plugins: [],
-}
+};
